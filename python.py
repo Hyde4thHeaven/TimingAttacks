4697adc6d0ffd16358c54c59c858a791eff30639
Hyde4thHeaven <74787546+Hyde4thHeaven@users.noreply.github.com>
2021-01-10 06:02:37 +0700

    Update python.py

--- python.py
+++ python.py
@@ -9,7 +9,7 @@ def check_authorized_unsecured(password):
         return False
   
 def check_authorized_secured(password):
-    if compare_digest(password, 'HYDEHEAVEN'):
+    if compare_digest(password, 'HYDEHEAVEN'*50):
         return True
     else:
         return False
@@ -18,25 +18,25 @@ print('=========================')
 
 password = 'HYPEHEAVEN'
 start = time.time()
-print(check_authorized_secured(password))
+print(check_authorized_unsecured(password))
 end = time.time()
 print('Time for HYPEHEAVEN (== operation:)', end - start)
 
 password = 'HYDEHAEVEN'*50
 start = time.time()
-print(check_authorized_secured(password))
+print(check_authorized_unsecured(password))
 end = time.time()
 print('Time for HYDEHAEVEN (== operation:)', end - start)
 
 password = 'HYDEHEAVEN'*50 + 'T'
 start = time.time()
-print(check_authorized_secured(password))
+print(check_authorized_unsecured(password))
 end = time.time()
 print('Time for HYDEHEAVENNNNT (== operation:)', end - start)
 
 password = 'HYDEHEAVEN'*50
 start = time.time()
-print(check_authorized_secured(password))
+print(check_authorized_unsecured(password))
 end = time.time()
 print('Time for HYDEHEAVEN (== operation:)', end - start)
 
